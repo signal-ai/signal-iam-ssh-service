7acc5e348631570cb69d2e11ee6cb2b449da0741
Sam Burrell <sam.burrell@signalmedia.co>
2018-04-12 09:44:57 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # signal-iam-ssh-service
-A script that utilises the sshd authorized_keys command to get public ssh keys from iam for ssh login
+A script that is executed by the sshd authorized_keys command to retreive active public ssh keys from IAM for a specific user for ssh login to AWS instances.
+
+## Intention
+We wanted a simple lightweight way of retrieving public keys with caching from IAM with no real granularity of role.
+
+A user logs in to an aws instances with the standard ec2-user. The offered public key is given to the script to check if it exists in the cache. If it exists in cache then that specific public key is verified in IAM to see if it is active. This minimises IAM calls and also gives a real truth check for each public key. If the public key is accepted then the user can log in.
+
+We log all of this including the users iam name to the systems secure syslog which can then be forwarded to any audit log you wish cloudwatch or other.
+
+## Use
+
+Place the script on the system in `/path/to/iam-ssh.py`
+
+Ensure the following lines are included in the sshd config:
+```
+AuthorizedKeysCommand /path/to/iam-ssh.py %k
+AuthorizedKeysCommandUser nobody
+```
+The `%k` is the base64-encoded key or certificate for authentication. from [sshd](https://man.openbsd.org/sshd_config)
+
+Ensure the script has the following permisions:
+
+```
+owner=root group=root mode=0755
+```
+
+Ensure the following varible is set correctly in the script for your systmes syslog
+```
+SYSLOG_ADDRESS = '/dev/log'
+```
---
+++ iam-ssh.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import boto3
+import sys, logging
+import logging.handlers
+import pickle
+
+
+SYSLOG_ADDRESS = '/dev/log' #for LINUX: /dev/log for OSX /var/run/syslog
+CACHE_FILE = '/tmp/iam-cache.p'
+
+# Set up logging to /var/log/secure
+logger = logging.getLogger('iam-ssh')
+logger.setLevel(logging.INFO)
+handler = logging.handlers.SysLogHandler(address=SYSLOG_ADDRESS, facility='authpriv')
+formatter = logging.Formatter('%(name)s: %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+
+def get_public_key(item):
+  '''Function to add key to an array of known allowed keys from AWS'''
+  try:
+    response = client.get_ssh_public_key(
+      UserName=item['UserName'],
+      SSHPublicKeyId=item['SSHPublicKeyId'],
+      Encoding='SSH'
+    )
+    return {
+    'UserName': response['SSHPublicKey']['UserName'],
+    'SSHPublicKey': response['SSHPublicKey']['SSHPublicKeyBody'],
+    'SSHPublicKeyId': response['SSHPublicKey']['SSHPublicKeyId'],
+    'Status': response['SSHPublicKey']['Status']
+  }
+  except:
+    logger.info('Could not find key for user: {}'.format(item['UserName']))
+    return None
+
+
+def fill_cache():
+  users = client.list_users()
+  cache = []
+  for user in users['Users']:
+    list_ssh_keys = client.list_ssh_public_keys(UserName=user['UserName'])
+    for item in list_ssh_keys['SSHPublicKeys']:
+      key = get_public_key(item)
+      if key:
+        cache.append(key)
+  pickle.dump(cache, open(CACHE_FILE,'wb'))
+  return cache
+
+def search_for_active_key(key, cache):
+  keys_to_check = list(filter(lambda k: key_to_check in k['SSHPublicKey'], cache))
+  allowed_keys = map(lambda k: get_public_key(k), keys_to_check)
+  result = list(filter(lambda k: k != None and key_to_check in k['SSHPublicKey'], allowed_keys))
+  if result:
+    for item in result:
+      if 'Active' in item['Status']:
+        logger.info('Public key found in IAM for user: {}'.format(item['UserName']))
+        print(item['SSHPublicKey'])
+      else:
+        logger.info('Public key found in IAM for user but user not active: {}'.format(item['UserName']))
+    return True
+  else:
+    return False
+
+
+if len(sys.argv) > 1:
+  key_to_check = sys.argv[1]
+  client = boto3.client('iam')
+  try:
+    cache = pickle.load(open(CACHE_FILE, 'rb'))
+  except:
+    cache = fill_cache()
+  if not search_for_active_key(key_to_check, cache):
+    cache = fill_cache()
+    if not search_for_active_key(key_to_check, cache):
+      logger.info('No user in IAM with active public key matching {}'.format(sys.argv[1]))
+else:
+  logger.info('No argument passed to iam-ssh.py')
